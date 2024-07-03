d3342ccb8f7beec9aea275b35679f0c4569d59d2
hoangduognguyxn <148853420+hoangduognguyxn@users.noreply.github.com>
2024-07-03 23:37:22 +0700

    Add files via upload

---
+++ bai29array_double_1d.cs
@@ -0,0 +1,39 @@
+﻿public class array_double_1d
+{
+    private double[] numbers;
+
+    public void setNumbers(double[] arr)
+    {
+        this.numbers = arr;
+    }
+
+    public double FindMin()
+    {
+        double min = this.numbers[0];
+
+        for (int i = 1; i < this.numbers.Length; i++)
+        {
+            if (this.numbers[i] < min)
+            {
+                min = this.numbers[i];
+            }
+        }
+
+        return min;
+    }
+
+    public double FindMax()
+    {
+        double max = this.numbers[0];
+
+        for (int i = 1; i < this.numbers.Length; i++)
+        {
+            if (this.numbers[i] > max)
+            {
+                max = this.numbers[i];
+            }
+        }
+
+        return max;
+    }
+}
